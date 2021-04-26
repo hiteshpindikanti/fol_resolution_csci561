0644136f1dc4613b003d4f0043ed01d48819817c
hiteshpindikanti <hiteshpindikanti@gmail.com>
2021-04-25 19:14:30 -0700

    added comments and additional test cases

--- main.py
+++ main.py
@@ -28,25 +28,25 @@ def run_all():
               .format(index, 'PASS' if expected_output == actual_output else 'FAIL', time_taken[index - 1]))
 
 
-def run_all2():
+def run_all2(folder: str):
     input_file_format = "input"
     expected_output_file_format = "expected_output"
     output_file_format = "output"
 
     time_taken = []
-    num_files = len(list(filter(lambda x: x.startswith('input'), listdir('test_data2'))))
+    num_files = len(list(filter(lambda x: x.startswith('input'), listdir(folder))))
     for index in range(1, num_files + 1):
         start = time.time()
-        if index not in {3, 25, 11}:
-            ResolutionIterative(input_file='test_data2/{}{}.txt'.format(input_file_format, index),
-                                output_file='test_data2/{}{}.txt'.format(output_file_format, index))
+        # if index not in {3, 25, 11}:
+        ResolutionIterative(input_file='{}/{}{}.txt'.format(folder, input_file_format, index),
+                            output_file='{}/{}{}.txt'.format(folder, output_file_format, index))
         end = time.time()
         time_taken.append(end - start)
 
     for index in range(1, num_files + 1):
-        with open('test_data2/{}{}.txt'.format(expected_output_file_format, index)) as file:
+        with open('{}/{}{}.txt'.format(folder, expected_output_file_format, index)) as file:
             expected_output = file.read().strip()
-        with open('test_data2/{}{}.txt'.format(output_file_format, index)) as file:
+        with open('{}/{}{}.txt'.format(folder, output_file_format, index)) as file:
             actual_output = file.read().strip()
 
         print("Test Case {}: {}, Time Taken: {}"
@@ -54,10 +54,10 @@ def run_all2():
 
 
 if __name__ == "__main__":
-    print("--SET 1--")
-    run_all()
-    print("--SET 2--")
-    run_all2()
-    # ResolutionIterative(input_file='test_data/input24.txt', output_file='test_data/output24.txt')
+    # print("--SET 1--")
+    # run_all()
+    # print("--SET 2--")
+    #run_all2(folder='test_data3')
+    ResolutionIterative(input_file='test_data/_input0.txt', output_file='test_data/_output0.txt')
     # ResolutionIterative(input_file='test_data/input25.txt', output_file='test_data/output25.txt')
-    #ResolutionIterative(input_file='test_data2/input24.txt', output_file='test_data2/output24.txt')
+    # ResolutionIterative(input_file='test_data2/input24.txt', output_file='test_data2/output24.txt')
--- resolution_iterative2.py
+++ resolution_iterative2.py
@@ -15,7 +15,13 @@ class Resolution:
         self.write_output(output_file, result)
         pass
 
-    def read_input(self, input_file):
+    def read_input(self, input_file: str):
+        """
+        Reads the input file and updates the class instance variables
+        :param input_file: input file name with path
+        :return:
+        """
+
         with open(input_file, 'r+') as file:
             self.num_query = int(file.readline())
             queries = []
@@ -57,26 +63,48 @@ class Resolution:
 
     @staticmethod
     def write_output(output_file: str, data: str):
+        """
+        Writes the result in the output file
+        :param output_file: output filename with path
+        :param data: string data to be written
+        :return:
+        """
+
         with open(output_file, 'w+') as file:
             file.write(data)
 
     @staticmethod
-    def dict_hash(d):
-        if isinstance(d, (list, tuple)):
-            return hash(tuple([Resolution.dict_hash(e) for e in d]))
-        elif isinstance(d, (set,)):
-            return hash(tuple([Resolution.dict_hash(e) for e in sorted(d)]))
-        elif not isinstance(d, (dict, defaultdict)):
-            return hash(d)
+    def any_hash(obj) -> int:
+        """
+        any_hash is a recursive hashing function which creates hash of
+        any mutable or immutable object passed to it.
+        :param obj:
+        :return: int hash value of the object
+        """
 
-        new_d = deepcopy(d)
-        for k, v in new_d.items():
-            new_d[k] = Resolution.dict_hash(v)
+        if isinstance(obj, (list, tuple)):
+            return hash(tuple([Resolution.any_hash(e) for e in obj]))
+        elif isinstance(obj, (set,)):
+            return hash(tuple([Resolution.any_hash(e) for e in sorted(obj)]))
+        elif not isinstance(obj, (dict, defaultdict)):
+            return hash(obj)
 
-        return hash(tuple(frozenset(sorted(new_d.items()))))
+        new_obj = deepcopy(obj)
+        for k, v in new_obj.items():
+            new_obj[k] = Resolution.any_hash(v)
+
+        return hash(tuple(frozenset(sorted(new_obj.items()))))
 
     @staticmethod
     def get_negative_query(query) -> tuple:
+        """
+        Returns the negated query.
+        :param query: query in the format (predicate, arguments_sequence)
+        :return:
+        Note: arguments_sequence is not being used in the implementation.
+        You can pass anything in it.
+        """
+
         if query[0][0] == '~':
             negative_query = (query[0][1:], query[1])
         else:
@@ -85,6 +113,17 @@ class Resolution:
 
     @staticmethod
     def is_unifiable(arguments1: tuple, arguments2: tuple) -> tuple:
+        """
+        Checks if two argument sequences are unifiable or not.
+        E.g. ~A(x,y) v B(x) AND A(H, M) v C(z)
+        arguments1 = (x, y), arguments2 = (H, M)
+        It will return statement1_assignments = {x: H, y: M}, statement2_assignments = {}
+
+        :param arguments1: argument sequence of 1st expression
+        :param arguments2: argument sequence of 2nd expression.
+        :return: (can_be_unified: bool, statement1_assignment2: dict, statement2_assignments: dict)
+        """
+
         if len(arguments1) != len(arguments2):
             return False, None, None
         arguments1 = list(arguments1)
@@ -110,7 +149,6 @@ class Resolution:
                         arg2_assignments[variable_name] = arguments1[i]
 
             elif arguments1[i][0].islower() and arguments2[i][0].isupper():
-                # This Case should never come in our project
                 variable_name = arguments1[i]
                 for j in range(len(arguments1)):
                     if arguments1[j] == variable_name:
@@ -162,6 +200,17 @@ class Resolution:
 
     @staticmethod
     def perform_assignment(statement: dict, assignment: dict, prev_statement=None):
+        """
+        Perform assignment on the statement according to the assignment and prev_statement, if given
+        Also performs Standardization of variables if required
+
+        :param statement: statement on which assignment has to be performed
+        :param assignment:
+        :param prev_statement: Used for standardization of variables,
+        basically it will to make any common variables as unique
+        :return: the assigned statement
+        """
+
         # Check variable name used in prev_statement, if exists
         prev_statement_variables = set()
         if prev_statement is not None:
@@ -188,27 +237,14 @@ class Resolution:
         return assigned_statement
 
     @staticmethod
-    def unify(statement1: dict, statement2: dict) -> dict:
-        for predicate, arguments_set in deepcopy(statement1).items():
-            negative_predicate, _ = Resolution.get_negative_query((predicate, tuple()))
-            if negative_predicate in statement2.keys():
-                for args in deepcopy(statement2[negative_predicate]):
-                    if args in arguments_set:
-                        statement1[predicate].remove(args)
-                        statement2[negative_predicate].remove(args)
+    def unify(statement1, statement2):
+        """
+        Perfom full unification including substitutions and eliminations of expressions
+        :param statement1:
+        :param statement2:
+        :return: unified statement
+        """
 
-        unified_statement = defaultdict(set)
-        for predicate, arguments_set in statement1.items():
-            if arguments_set:
-                unified_statement[predicate].update(arguments_set)
-        for predicate, arguments_set in statement2.items():
-            if arguments_set:
-                unified_statement[predicate].update(arguments_set)
-
-        return unified_statement
-
-    @staticmethod
-    def unify2(statement1, statement2):
         # Keep unifying until no more unification exists
         flag = True
         while flag:
@@ -276,6 +312,11 @@ class Resolution:
         return unified_statement
 
     def apply_resolution(self, initial_statement) -> bool:
+        """
+        Apply resolution starting from the initial statement
+        :param initial_statement:
+        :return: boolean value if the requested query is True or False
+        """
 
         predicate, arguments_set = next(iter(initial_statement.items()))
         arguments = next(iter(arguments_set))
@@ -285,16 +326,21 @@ class Resolution:
         stack = [(initial_statement, query, statement, arguments)
                  for statement in self.knowledge_base[negative_query[0]]
                  for arguments in statement[negative_query[0]]]
+
+        # Iterative recursion implementation for handling greater depths
         while stack:
             initial_statement, query, statement, arguments = stack.pop(-1)
-            visited.add(self.dict_hash((initial_statement, query, statement, arguments)))
+            visited.add(self.any_hash((initial_statement, query, statement, arguments)))
 
             negative_query = Resolution.get_negative_query(query)
 
             unifiable, _, _ = Resolution.is_unifiable(negative_query[1], arguments)
             if unifiable:
                 # Unify Statements
-                unified_statement = Resolution.unify2(initial_statement, statement)
+                unified_statement = Resolution.unify(initial_statement, statement)
+
+                print("Initial Statement: {}\nKB Statement: {}\nUnified: {}\n"
+                      .format(dict(initial_statement), dict(statement), dict(unified_statement)))
 
                 if not unified_statement:
                     return True
@@ -308,11 +354,16 @@ class Resolution:
 
                 for statement in self.knowledge_base[negative_query[0]]:
                     for arguments in statement[negative_query[0]]:
-                        if self.dict_hash((initial_statement, query, statement, arguments)) not in visited:
+                        if self.any_hash((initial_statement, query, statement, arguments)) not in visited:
                             stack.append((initial_statement, query, statement, arguments))
         return False
 
     def resolve_queries(self) -> str:
+        """
+        Resolve the queries one by one
+        :return: Boolean Array Str data for all the processed queries
+        """
+
         result = []
         for query in self.queries:
             negative_query = Resolution.get_negative_query(query)
